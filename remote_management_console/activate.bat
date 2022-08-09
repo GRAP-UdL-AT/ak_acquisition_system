ee818d145617451e91ce9d84ac460ddb6a6fae99
Juan Carlos <43625026+juancarlosmiranda@users.noreply.github.com>
2022-08-09 11:51:04 +0200

    Fix documentation
    Fix documentation

--- remote_management_console/activate.bat
+++ remote_management_console/activate.bat
@@ -1,3 +1,3 @@
 @echo off
 rem .\venv\Scripts\activate.bat
-%userprofile%\development_env\remote_management_console-venv\Scripts\activate.bat & python ui_console_manager.py
+%userprofile%\development_env\remote_management_console-venv\Scripts\activate.bat & python main_management_console.py
